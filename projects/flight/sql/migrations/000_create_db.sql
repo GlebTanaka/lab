857f72be0eb0f0816c882de28d64b632f3723081
Gleb.Tanaka <69252964+GlebTanaka@users.noreply.github.com>
2025-08-29 11:45:10 +0200

    romved dublicates from sql orphan branch

--- projects/flight/sql/migrations/000_create_db.sql
+++
@@ -1,5 +0,0 @@
--- Create a role for the project
-CREATE ROLE skyhub_owner LOGIN PASSWORD 'change_me';
-
--- Create the project database and assign ownership
-CREATE DATABASE skyhub_db OWNER skyhub_owner;
--- sql-learning/postgresql/01_module/001_create_a_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE product (
-    product_name VARCHAR(255),
-    price INT
-);
